667144aa2f0808fa03639f09044ba4e337e070bd
Monica Ayala Marrero <a01707439@itesm.mx>
2022-03-21 04:05:06 +0000

    added syntax error handler

--- main.cpp
+++ main.cpp
@@ -4,12 +4,16 @@
 #include <vector>
 
 using namespace std;
+
+ofstream MyFile("output.txt");
 string s="1234567890";
 string a="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
 string u="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890_";
 void lexerAritmetico(string archivo) {
+  archivo = " " + archivo;
+  
 
-  cout<<"Token                                  |Type"<<endl<<
+  MyFile<<"Token                                  |Type"<<endl<<
 "--------------------------------------------------------------------------------"<<endl;
   
   for(std::string::size_type i = 0; i < archivo.size();i++){
@@ -17,15 +21,15 @@ void lexerAritmetico(string archivo) {
 //OPERADORES
   // SUMA
     if(archivo[i]== '+'){
-      cout << archivo[i] << string(38, ' ') << "|SUMA" <<endl<<"--------------------------------------------------------------------------------"<<endl;   
+      MyFile << archivo[i] << string(38, ' ') << "|SUMA" <<endl<<"--------------------------------------------------------------------------------"<<endl;   
     }
   // RESTA
     if(archivo[i]== '-'){
-      cout << archivo[i] << string(38, ' ') << "|SUBSTRACTION" <<endl<<"--------------------------------------------------------------------------------"<<endl;   
+      MyFile << archivo[i] << string(38, ' ') << "|SUBSTRACTION" <<endl<<"--------------------------------------------------------------------------------"<<endl;   
     }
   // MULTIPLICACIÓN
     if(archivo[i]== '*'){
-      cout << archivo[i] << string(38, ' ') << "|MULTIPLICATION" <<endl<<"--------------------------------------------------------------------------------"<<endl;    
+      MyFile << archivo[i] << string(38, ' ') << "|MULTIPLICATION" <<endl<<"--------------------------------------------------------------------------------"<<endl;    
     }
   // DIVISIÓN
     if(archivo[i]== '/'){
@@ -45,41 +49,46 @@ void lexerAritmetico(string archivo) {
           // } else { cout<<aux<< " |COMMENT"<<endl<<"--------------------------------------------------------------------------------"<<endl;} 
       }
       else{
-         cout << archivo[i] << string(38, ' ') << "|DIVISION" <<endl<<"--------------------------------------------------------------------------------"<<endl;      
+         MyFile << archivo[i] << string(38, ' ') << "|DIVISION" <<endl<<"--------------------------------------------------------------------------------"<<endl;      
       }
      
     }
   // POTENCIA
     if(archivo[i]== '^'){
-      cout << archivo[i] << string(38, ' ') << "|POWER" <<endl<<"--------------------------------------------------------------------------------"<<endl;      
+      MyFile << archivo[i] << string(38, ' ') << "|POWER" <<endl<<"--------------------------------------------------------------------------------"<<endl;      
     }
   // ASIGNACIÓN 
     if(archivo[i]== '='){
-      cout << archivo[i] << string(38, ' ') << "|ASSIGNMENT" <<endl<<"--------------------------------------------------------------------------------"<<endl;    
+      MyFile << archivo[i] << string(38, ' ') << "|ASSIGNMENT" <<endl<<"--------------------------------------------------------------------------------"<<endl;    
     }
 
 //PARÉNTESIS
   // QUE ABREN
     if(archivo[i]== '('){
-      cout << archivo[i] << string(38, ' ') << "|LEFT PARENTHESIS" <<endl<<"--------------------------------------------------------------------------------"<<endl;     
+      MyFile << archivo[i] << string(38, ' ') << "|LEFT PARENTHESIS" <<endl<<"--------------------------------------------------------------------------------"<<endl;     
     }
   // QUE CIERRAN
     if(archivo[i]== ')'){
-      cout << archivo[i] << string(38, ' ') << "|RIGHT PARENTHESIS" <<endl<<"--------------------------------------------------------------------------------"<<endl;    
+      MyFile << archivo[i] << string(38, ' ') << "|RIGHT PARENTHESIS" <<endl<<"--------------------------------------------------------------------------------"<<endl;    
     }
     
 //VARIABLE
   if(a.find(archivo[i])<a.length()){
+    
     string aux;
     aux = aux + archivo[i];
     while(u.find(archivo[i+1])<u.length()){
       aux = aux + archivo[i+1];
       i++;
     }
+    if(archivo[i-1]!=' ' &&  archivo[i-1]!='\n'  &&  archivo[i-1]!='('  &&  archivo[i-1]!='+'  &&  archivo[i-1]!='-'  &&  archivo[i-1]!='^'  &&  archivo[i-1]!='/'  &&  archivo[i-1]!='*' ){
+      MyFile << " ' "<< archivo[i-1]<<aux<< " '  "<<"SYNTAX ERROR " <<endl;
+      cout << "SYNTAX ERROR MODIFY FILE " << endl << "Error at char number: "<< to_string(i)<<endl;    }
+    else{
     int len = 39 - aux.length();
     if(len >= 0){
-    cout << aux << string(len, ' ') << "|VARIABLE" <<endl<<"--------------------------------------------------------------------------------"<<endl;
-    } else { cout<<aux<< " |VARIABLE"<<endl<<"--------------------------------------------------------------------------------"<<endl;} 
+    MyFile << aux << string(len, ' ') << "|VARIABLE" <<endl<<"--------------------------------------------------------------------------------"<<endl;
+    } else { MyFile<<aux<< " |VARIABLE"<<endl<<"--------------------------------------------------------------------------------"<<endl;} }
   }
     
 //NÚMERO
@@ -87,6 +96,7 @@ void lexerAritmetico(string archivo) {
   //ENTERO
     //inicia con digito o con -
     if(s.find(archivo[i])<s.length() || (archivo[i]=='-' && (s.find(archivo[i+1])<s.length()))){
+      bool interger = false;
       string aux; aux += archivo[i];
       while(s.find(archivo[i+1])<s.length()){
         aux = aux + archivo[i+1];
@@ -114,17 +124,33 @@ void lexerAritmetico(string archivo) {
             i++;
           }
         }
-        int len = 39 - aux.length();
-        if(len >= 0){
-    cout << aux << string(len, ' ') << "|REAL" <<endl<<"--------------------------------------------------------------------------------"<<endl;
-    } else { cout<<aux<< " |REAL"<<endl<<"--------------------------------------------------------------------------------"<<endl;} //END STATE
+        else if((archivo[i+1]=='e' || archivo[i+1]== 'E') && !( (s.find(archivo[i+2])<s.length()) || (archivo[i+2] == '-') )){
+          MyFile<< aux << " ''" << archivo[i+1] << archivo[1+2]<< " SYNTAX ERROR"<<" ''" <<endl;
+        }
       }
       else{
-        int len = 39 - aux.length();
-        if(len >= 0){
-    cout << aux << string(len, ' ') << "|INTERGER" <<endl<<"--------------------------------------------------------------------------------"<<endl;
-    } else { cout<<aux<< " |INTERGER"<<endl<<"--------------------------------------------------------------------------------"<<endl;}
+        interger = true;
       }
+
+      if(archivo[i+1]!=' ' && archivo[i+1]!='\n' && archivo[i+1]!=')'&& archivo[i+1]!='+'&& archivo[i+1]!='-' && archivo[i+1]!='*'&& archivo[i+1]!='/' && archivo[i+1]!='='&& archivo[i+1]!='^'){  
+          MyFile << " ' "<< aux<<archivo[i+1]<< " '  "<<"SYNTAX ERROR " <<endl;
+        cout << "SYNTAX ERROR MODIFY FILE " << endl << "Error at char number: "<< to_string(i)<<endl;  
+        } else{
+        //REAL
+        if(!interger){
+          int len = 39 - aux.length();
+          if(len >= 0){
+          MyFile << aux << string(len, ' ') << "|REAL" <<endl<<"--------------------------------------------------------------------------------"<<endl;
+          } else { MyFile<<aux<< " |REAL"<<endl<<"--------------------------------------------------------------------------------"<<endl;} 
+        }else{
+          int len = 39 - aux.length();
+          if(len >= 0){
+          MyFile << aux << string(len, ' ') << "|INTERGER" <<endl<<"--------------------------------------------------------------------------------"<<endl;}        
+          else { MyFile <<aux<< " |INTERGER"<<endl<<"--------------------------------------------------------------------------------"<<endl;}
+        }
+        
+        
+        }
     }
   }
 }
